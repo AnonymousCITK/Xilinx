2f7dff6d07fbee506a7a6821531f92c0f511cab9
Tandeep Das <137149834+tandeep-das@users.noreply.github.com>
2023-09-01 14:26:26 +0530

    Add files via upload

---
+++ FullAdder/FullAdder.srcs/sim_1/new/t_fA.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.09.2023 14:22:33
+// Design Name: 
+// Module Name: t_fA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module t_fA(
+
+    );
+endmodule
